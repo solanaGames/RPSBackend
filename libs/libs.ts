65dde8ea5cd999b73f1ea588030533d23286e03d
weston <wmizumoto@gmail.com>
2023-02-02 15:04:50 -0800

    add libs folder

---
+++ libs/libs.ts
@@ -0,0 +1,4 @@
+import { RedisClient } from './redis/client';
+export { 
+    RedisClient
+}
---
+++ libs/redis/client.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RedisClient = void 0;
+class RedisClient {
+    constructor() {
+    }
+}
+exports.RedisClient = RedisClient;
